cce221c9a931db9edce46edd0c2ece9c012dfd99
Ranika Williams <rnkprice07@gmail.com>
2019-05-08 01:58:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
-
+  def oxford_comma(array)
+    if array.size == 1
+      return array[0]
+    elsif array.size == 2
+      array.insert(1,"and")
+      array.join(" ")
+    elsif array.size >= 3
+      last_element = array.pop
+      array.push("and " + last_element)
+      array.join(", ")
+  end
 end
